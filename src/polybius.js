a9e8b96da913e9cb69db802458c13f10d21c4dbf
Jordan Combs <jrdncombs@gmail.com>
2023-08-10 07:46:41 -0500

    all

--- src/polybius.js
+++ src/polybius.js
@@ -7,7 +7,58 @@ const polybiusModule = (function () {
   // you can add any code you want within this function scope
 
   function polybius(input, encode = true) {
-    // your solution code here
+    const polybiusSquare = [
+      ["a", "b", "c", "d", "e"],
+      ["f", "g", "h", "i/j", "k"],
+      ["l", "m", "n", "o", "p"],
+      ["q", "r", "s", "t", "u"],
+      ["v", "w", "x", "y", "z"]
+    ];
+  
+    input = input.toLowerCase();
+    let result = "";
+  
+    if (encode) {
+      for (let i = 0; i < input.length; i++) {
+        const char = input[i];
+        if (char === " ") {
+          result += char;
+        } else if (char === "i" || char === "j") {
+          result += "42";
+        } else {
+          for (let row = 0; row < polybiusSquare.length; row++) {
+            const column = polybiusSquare[row].indexOf(char);
+            if (column !== -1) {
+              result += `${column + 1}${row + 1}`;
+              break;
+            }
+          }
+        }
+      }
+    }
+  
+    if (!encode) {
+      if (input.replace(/\s/g, '').length % 2 !== 0) {
+        return false;
+      } else {
+        for (let i = 0; i < input.length; i++) {
+          const char = input[i];
+          if (char === " ") {
+            result += char;
+          } else if (char === "4" && input[i + 1] === "2") {
+            result += "(i/j)";
+            i++;
+          } else {
+            const row = parseInt(input[i + 1]) - 1;
+            const col = parseInt(char) - 1;
+            result += polybiusSquare[row][col];
+            i++;
+          }
+        }
+      }
+    }
+    
+    return result;
   }
 
   return {
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,38 @@
-// Write your tests here!
+const { expect } = require('chai');
+const { polybius } = require('../src/polybius.js');
+
+describe('Polybius Module', () => {
+  describe('Encoding', () => {
+    it('should encode the letters i and j to 42', () => {
+      const encoded = polybius('ij');
+      expect(encoded).to.equal('4242');
+    });
+
+    it('should ignore capital letters', () => {
+      const encodedUpper = polybius('A Message');
+      const encodedLower = polybius('a message');
+      expect(encodedUpper).to.equal(encodedLower);
+    });
+
+    it('should maintain spaces in the message', () => {
+      const encoded = polybius('a message');
+      const expected = '11 23513434112251';
+      expect(encoded).to.equal(expected);
+    });
+  });
+
+  describe('Decoding', () => {
+    it('should decode 42 to (i/j)', () => {
+      const decoded = polybius('4242', false);
+      expect(decoded).to.equal('(i/j)(i/j)');
+    });
+
+    it('should maintain spaces in the decoded message', () => {
+      const decoded = polybius('11 23513434112251', false);
+      const expected = 'a message';
+      expect(decoded).to.equal(expected);
+    });
+  });
+});
+
+
